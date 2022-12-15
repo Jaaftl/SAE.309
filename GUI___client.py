3783e44d68175a9a7e7b3b49c1ec55bf231689bd
Jaaftl <106062441+Jaaftl@users.noreply.github.com>
2022-12-15 10:59:57 +0100

    Update GUI___client.py

--- GUI___client.py
+++ GUI___client.py
@@ -1,85 +1,179 @@
-
 import socket
 import time
-import psutil
-import subprocess
-
-def serveur():
-
-    print('démarrage du serveur...')
-    host = '127.0.0.1' #socket.gethostname()
-    port = 10017
-
-    server_socket = socket.socket()
-    server_socket.bind((host, port))
-    time.sleep(2)
-    print('serveur démarré')
-    print(host, port)
-
-    server_socket.listen(2)
-    conn, address = server_socket.accept()
-    print("connexion depuis" + str(address))
-
-    time.sleep(2)
-
-    msg = ''
-    while msg != 'kill':
-        data = conn.recv(1024).decode()
-        if data == "IP" or  data == "OS" or data == "NAME":
-            if data == 'OS':
-                cmd = 'ver'
-            elif data == 'IP':
-                cmd = 'ipconfig | findstr /i "ipv4" '
-            elif data == "NAME":
-                cmd = 'hostname'
-            p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='cp850', shell=True)
-            cmd = p.stdout.read()
-            conn.send(cmd.encode())
-            # la sortie et erreur sont récuperées par les attributs stdout et stderr. faite un read()
-            #print(f"résultat commande : \n {p.stdout.read()}")
-        if data == "CPU":
-            cpu = f"Utilisation de CPU : {psutil.cpu_percent()} %"
-            conn.send(cpu.encode())
-        if data == "RAM":
-            total = f"RAM totale : {round(((psutil.virtual_memory()[0]) / 1000000000),2)} Go"
-            libre = f"RAM libre : {round(((psutil.virtual_memory()[4]) / 1000000000),2)} Go"
-            utilise = f"RAM utilisés : {round(((psutil.virtual_memory()[3]) / 1000000000),2)} Go"
-            conn.send(total.encode())
-            conn.send(libre.encode())
-            time.sleep(0.2)
-            conn.send(utilise.encode())
-        if data == 'disconnect' or data == 'reset':
-            ok = 'ok, bye !'
-            conn.send(ok.encode())
-            print("Depuis le client: " + str(data))
-            print('client déconnecté')
-            if data == 'reset':
-                print('redémarrage du serveur...')
-                conn.close()
-                time.sleep(0.5)
-                print('démarrage du serveur...')
-                host = '127.0.0.1' #socket.gethostname()
-                port = 10017
-                server_socket = socket.socket()
-                server_socket.bind((host, port))
-                time.sleep(0.5)
-                print('serveur démarré')
-            server_socket.listen(2)
-            conn, address = server_socket.accept()
-            print("connexion depuis" + str(address))
-
-        if data == 'kill':
-            msg = 'kill'
-            print("Depuis le client: " + str(data))
-            ok = "j'arrête !"
-            conn.send(ok.encode())
-
-            time.sleep(0.5)
-
-    conn.close()
+import threading
+from PyQt5.QtWidgets import QApplication, QWidget, QTextEdit, QGridLayout, QPushButton, QLineEdit, QLabel, QMessageBox, QFileDialog
+import sys
+import csv
 
 
+class Client(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.resize(700, 500)
+        self.__thread = None
+        self.setWindowTitle("Tchat")
 
-if __name__ == '__main__' :
-    serveur()
 
+        self.__IP = QLabel("IP : ")
+        self.__ip = QLineEdit("127.0.0.1")
+
+        self.__PORT = QLabel("PORT : ")
+        self.__port = QLineEdit('10017')
+
+        self.btnconnect = QPushButton("Connect")
+
+        self.lirecsv = QPushButton("open file")
+
+        self.etat = QLabel()
+        self.etat.setText('déconnecté')
+        self.panneau = QTextEdit()
+        self.panneau.setEnabled(False)
+        self.__text = QLineEdit("")  # Entrée message du client
+
+        self.ajout_message = QPushButton("Add message")
+        self.ajout_message.setEnabled(False)
+        self.effacer = QPushButton("Clear")
+        self.effacer.setEnabled(False)
+
+        layout = QGridLayout()
+        layout.addWidget(self.__IP, 0, 0)
+        layout.addWidget(self.__ip, 0, 1)
+        layout.addWidget(self.lirecsv, 0, 3)
+        layout.addWidget(self.__PORT, 1, 0)
+        layout.addWidget(self.__port, 1, 1)
+        layout.addWidget(self.btnconnect, 2, 1)
+
+        layout.addWidget(self.etat, 3, 0)
+        layout.addWidget(self.panneau, 3, 1)
+        layout.addWidget(self.__text, 4, 1)
+        layout.addWidget(self.ajout_message, 4, 3)
+        layout.addWidget(self.effacer, 6, 1)
+        self.setLayout(layout)
+
+        self.btnconnect.clicked.connect(self.connexion)
+        self.effacer.clicked.connect(self.__effacer)
+
+        self.ajout_message.clicked.connect(self.__envoyer)
+        self.lirecsv.clicked.connect(self.__lirecsv)
+
+
+    def connexion(self):
+        x = str(self.etat.text())
+        if x == "déconnecté":
+            try:
+                self.__client_socket = socket.socket()
+                IP = str(self.__ip.text())
+                PORT = int(self.__port.text())
+                self.__client_socket.connect((IP, PORT))
+            except ConnectionRefusedError:
+                print("serveur non lancé ou mauvaise information")
+                erreur = QMessageBox()
+                erreur.setWindowTitle("Erreur")
+                erreur.setText("serveur non lancé ou mauvaise information")
+                erreur.resize(250, 500)
+                erreur.setIcon(QMessageBox.Critical)
+
+                erreur.exec_()
+            except ConnectionError:
+                print("erreur de connection")
+                erreur = QMessageBox()
+                erreur.setWindowTitle("Erreur")
+                erreur.setText("Erreur de connection")
+                erreur.resize(250, 500)
+                erreur.setIcon(QMessageBox.Critical)
+                erreur.exec_()
+
+            except OSError:
+                print("Erreu d'IP ou port")
+                erreur = QMessageBox()
+                erreur.setWindowTitle("Erreur")
+                erreur.setText("l'IP ou le port n'est pas correct")
+                erreur.resize(250, 500)
+                erreur.setIcon(QMessageBox.Critical)
+                erreur.exec_()
+
+            except TypeError:
+                print("le fromat du saisie est faux")
+                erreur = QMessageBox()
+                erreur.setWindowTitle("Erreur")
+                erreur.setText("le fromat du saisie est faux")
+                erreur.resize(250, 500)
+                erreur.setIcon(QMessageBox.Critical)
+                erreur.exec_()
+
+            else:
+                print("connecté au serveur")
+                self.etat.setText('connecté')
+                self.effacer.setEnabled(True)
+                self.ajout_message.setEnabled(True)
+                self.__thread = threading.Thread(target=self.__msg_du_serv, args=[self.__client_socket])
+                self.__thread.start()
+                print("thread lancé")
+        elif x == "connecté":
+            pass
+
+    def __envoyer(self):
+        x = self.__text.text()
+        x = str(x)
+        self.__client_socket.send(x.encode())
+        self.panneau.append(x)
+        self.__text.clear()
+        #if x == 'disconnect' or x == 'reset' or x == 'kill':
+            #pass
+
+    def __effacer(self):
+        self.panneau.setPlainText("")
+
+
+
+
+    def __msg_du_serv(self, conn):
+        print('task start')
+        data = ""
+        while data != 'stop':
+            data = conn.recv(1024).decode()
+            if data == 'ok, bye !' or data == 'ok, je redémarre !' or data == "j'arrête !":
+                self.panneau.append(data)
+                time.sleep(0.1)
+                data = 'stop'
+            else:
+                self.panneau.append(data)
+                time.sleep(0.1)
+        self.__client_socket.close()
+        self.etat.setText('déconnecté')
+        self.effacer.setEnabled(False)
+        self.ajout_message.setEnabled(False)
+        print("task fini")
+
+
+    def __lirecsv(self):
+        print("open dialog")
+        self.open_dialog_box()
+
+    def open_dialog_box(self):
+        filename = QFileDialog.getOpenFileName()
+        path = str(filename[0])
+
+        '''with open(path) as file:
+            reader = csv.reader(file)
+            next(reader)
+            data = []
+            for row in reader:
+                row[0] = IP.IPv4Address(row[0])
+                data.append(row)
+
+            data.sort()'''
+
+        reader = csv.reader(open(path), delimiter=',')
+
+        data = reader
+
+
+        print(data)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    win = Client()
+    win.show()
+    sys.exit(app.exec_())
